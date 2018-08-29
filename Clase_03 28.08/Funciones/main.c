d30b08316c8a515e38682672cde04bd44db4a327
alumno <alumno@LAB00PC00>
2018-08-28 21:41:53 -0300

    funcionDeEdad

--- Clase_03 28.08/Funciones/main.c
+++ Clase_03 28.08/Funciones/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int pedirEdad(void);
+int validarEdad(int, int, int);
+
 int main()
 {
-    printf("Hello world!\n");
+    int edad;
+
+    edad = pedirEdad();
+
+    printf("Edad guardada: %d", edad);
+
     return 0;
+}
+
+int validarEdad(int edad, int max, int min)
+{
+    int edadValidada=0;
+
+    if(edad<=min && edad>=max)
+    {
+        edadValidada=1;
+    }
+
+    return edadValidada;
+}
+
+int pedirEdad(void)
+{
+    int edad;
+    int edadValidada = 0;
+
+    do
+    {
+        printf("\nIngrese edad: ");
+        scanf("%d", &edad);
+        edadValidada=validarEdad(edad, 1, 100);
+
+    }while(edadValidada != 1);
+
+    return edad;
 }
