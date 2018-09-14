f2ef5e215fc2a939c8513a8c1ee7f106083fbcfd
alumno <alumno@LAB00PC00>
2018-09-13 22:14:50 -0300

    generar struct

--- Clase_07 11.09/EjemploDeLibreriaArray/main.c
+++ Clase_07 11.09/EjemploDeLibreriaArray/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "utn.h"
 
 
 #define CANTIDAD_EMPLEADOS 6
 
+int ordenarArray(char pNombres[][50]);
+int sortEmpleadosPorNombre(Empleado pEmpleado, int limite, int orden);
+
 int main()
 {
-    char nombres[CANTIDAD_EMPLEADOS][50];
+    char nombres[CANTIDAD_EMPLEADOS][50] = {"AAA", "CCC", "BBB"};
+    char apellidos[CANTIDAD_EMPLEADOS][50] = {"AAA", "CCC", "BBB"};
+    char dni[CANTIDAD_EMPLEADOS][50] = {"AAA", "CCC", "BBB"};
     int i;
 
     for(i=0;i < CANTIDAD_EMPLEADOS;i++)
@@ -20,14 +26,70 @@ int main()
 
     for(i=0;i < CANTIDAD_EMPLEADOS;i++)
     {
-        printf("%s\n", nombres[i]);
+        printf("%s - %s - %s\n", nombres[i]);
+    }
+
+    ordenarArray(nombres);
+   // ordenarPorNombre(nombres, apellidos, dni, CANTIDAD_EMPLEADOS)
+
+    printf("Ordenado\n\n");
+
+    for(i=0;i < CANTIDAD_EMPLEADOS;i++)
+    {
+        printf("%s - %s - %s\n", nombres[i], apellidos[i], dni[i]);
     }
 
     return 0;
 }
 
+int ordenarArray(char pNombres[][50])
+{
+    int i;
+    int j;
+    char auxBuffer[5000];
+
+    for(i=0; i<CANTIDAD_EMPLEADOS; i++)
+    {
+        for(j=i+1; j<CANTIDAD_EMPLEADOS; j++)
+        {
+            if(strcmp(pNombres[i], pNombres[j]) < 0)
+            {
+                strcpy(auxBuffer,pNombres[i]);
+                strcpy(pNombres[i], pNombres[j]);
+                strcpy(pNombres[j], auxBuffer);
+            }
+        }
+    }
+}
+
+int sortEmpleadosPorNombre(Empleado* pEmpleado, int limite, int orden);
+{
 
 
+}
+/*void ordenarPorNombre(char* pNombres, char* pApellidos, char* pDni, int limite)
+{
+    int swap;
+    int i;
+    char bufferStr[256];
 
+    do
+    {
+        swap = 0;
+        for(i=0; i<limite; i++)
+        {
+            if(strcmp(pNombres[i], pNombres[i+1]) > 0)
+            {
+                swap = 1;
 
+                strcpy(auxBuffer,pNombres[i]);
+                strcpy(pNombres[i], pNombres[j]);
+                strcpy(pNombres[j], auxBuffer
 
+                strcpy(auxBuffer,pApellidos[i]);
+                strcpy(pApellidos[i], pApellidos[j]);
+                strcpy(pApellidos[j], auxBuffer);
+            }
+        }
+    }while(swap);
+}*/
