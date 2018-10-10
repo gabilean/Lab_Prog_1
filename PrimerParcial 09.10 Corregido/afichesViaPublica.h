1dc1b9abf7d8064521afa424bd4c26ef08000bbd
alumno <alumno@LAB00PC00>
2018-10-09 21:43:26 -0300

    No modificar PrimerParcial 09.10 Corregido

---
+++ PrimerParcial 09.10 Corregido/afichesViaPublica.h
@@ -0,0 +1,6 @@
+#ifndef AFICHESVIAPUBLICA_H_INCLUDED
+#define AFICHESVIAPUBLICA_H_INCLUDED
+
+void afichesViaPublica_menu(void);
+
+#endif // AFICHESVIAPUBLICA_H_INCLUDED
---
+++ PrimerParcial 09.10 Corregido/main.c
@@ -0,0 +1,13 @@
+#include <stdio_ext.h>
+#include <string.h>
+#include <stdlib.h>
+#include "cliente.h"
+#include "afiches.h"
+#include "myLibrary.h"
+#include "afichesViaPublica.h"
+
+int main()
+{
+    afichesViaPublica_menu();
+    return 0;
+}
